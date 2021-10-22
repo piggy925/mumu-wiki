7cb5077a86b17f1fecd44d466d5c639f2e76788f
MuMu <piggy925@163.com>
2021-10-22 13:42:13 +0800

    feat：集成MyBatis框架

--- src/main/java/com/mumu/wiki/MumuwikiApplication.java
+++ src/main/java/com/mumu/wiki/MumuwikiApplication.java
@@ -1,9 +1,11 @@
 package com.mumu.wiki;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.mumu.wiki.mapper")
 public class MumuwikiApplication {
 
     public static void main(String[] args) {
