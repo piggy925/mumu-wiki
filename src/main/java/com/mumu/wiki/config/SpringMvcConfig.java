12e25d1a0016db5835ffa99af8b63843c1f92cf4
MuMu <piggy925@163.com>
2021-10-25 19:19:03 +0800

    refactor：后端去掉过滤器与拦截器

--- src/main/java/com/mumu/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/mumu/wiki/config/SpringMvcConfig.java
@@ -1,22 +1,22 @@
-package com.mumu.wiki.config;
-
-import com.mumu.wiki.interceptor.LoginInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-    @Resource
-    LoginInterceptor loginInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor)
-                .addPathPatterns("/**")
-                .excludePathPatterns(
-                        "/user/login"
-                );
-    }
-}
+//package com.mumu.wiki.config;
+//
+//import com.mumu.wiki.interceptor.LoginInterceptor;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//import javax.annotation.Resource;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//    @Resource
+//    LoginInterceptor loginInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(loginInterceptor)
+//                .addPathPatterns("/**")
+//                .excludePathPatterns(
+//                        "/user/login"
+//                );
+//    }
+//}
